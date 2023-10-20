85e4dce733b9ac44b48bcb7b349858c848daa4c2
Romain Beauxis <toots@rastageeks.org>
2023-10-20 00:20:35 -0500

    Add proper error logging.

--- src/liquidsoap_js.ml
+++ src/liquidsoap_js.ml
@@ -1,12 +1,19 @@
 open Js_of_ocaml
 open Liquidsoap_lang
 
-let () =
-  Hooks.regexp := Regexp_js.make
+let () = Hooks.regexp := Regexp_js.make
 
 let parse content =
   let content = Js.to_string content in
-  let json = Liquidsoap_tooling.Parsed_json.parse_string content in
+  let json =
+    let buf = Buffer.create 0 in
+    let formatter = Format.formatter_of_buffer buf in
+    try Liquidsoap_tooling.Parsed_json.parse_string ~formatter content
+    with _ ->
+      Js_error.raise_
+        (Js_error.of_error
+           (new%js Js.error_constr (Js.string (Buffer.contents buf))))
+  in
   Js._JSON##parse
     (Js.string (Liquidsoap_lang.Json.to_string ~compact:true json))
 
