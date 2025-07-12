678d70266f6b79869cf165e27afaeb72b6f150bb
Romain Beauxis <toots@rastageeks.org>
2025-07-12 11:29:05 -0500

    Fix webpack.

--- webpack.web.cjs
+++ webpack.web.cjs
@@ -1,9 +1,15 @@
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+
 module.exports = {
   entry: "./src/index.js",
   mode: "production",
   experiments: {
     outputModule: true,
   },
+  externals: {
+    "node:util": "commonjs util",
+  },
+  plugins: [new NodePolyfillPlugin()],
   output: {
     path: __dirname + "/dist",
     filename: "web.mjs",
